287bc386e487dd8255df08cab794053da459341c
ShawnGChristy <shawn.g.christy@gmail.com>
2017-01-13 17:09:01 -0600

    Associate user with posts and lessons. Navigate and authenticate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113172028) do
+ActiveRecord::Schema.define(version: 20170113224439) do
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170113172028) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
